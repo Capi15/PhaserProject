8a83f6168128ae4585fb9b819ca6d62c4d7ea70b
Capi15 <capicapi1515@gmail.com>
2021-01-07 14:55:03 +0000

    added a timer succesfully

--- JS/worldscene.js
+++ JS/worldscene.js
@@ -1,3 +1,20 @@
+// Variáveis globais
+let timer;
+let total = 0;
+let vida;
+let oxigenio;
+let temperatura;
+let moedas;
+let geracoes;
+let fonte;
+
+//Variáveis de texto da Barra de estado
+let vidaText;
+let oxigenioText;
+let temperaturaText;
+let moedasText;
+let geracoesText;
+
 class WorldScene extends Phaser.Scene {
     constructor() {
         super({ key: 'WorldScene' });
@@ -14,35 +31,35 @@ class WorldScene extends Phaser.Scene {
 
         //controlo de Vida
         const vidaImage = this.add.image(20, 20, 'vida').setScale(0.02);
-        const fonte = { font: '16px Arial', fill: '#ffffff' };
-        let vida = 100;
-        let vidaText = this.add.text(40, 10, vida + 'HP', fonte);
+        fonte = { font: '16px Arial', fill: '#ffffff' };
+        vida = 100;
+        vidaText = this.add.text(40, 10, vida + 'HP', fonte);
 
         //controlo de Oxigénio
         const oxigenioImage = this.add
             .image(120, 20, 'oxigenio')
             .setScale(0.05);
-        let oxigenio = 100;
-        let oxigenioText = this.add.text(140, 10, oxigenio + '%', fonte);
+        oxigenio = 100;
+        oxigenioText = this.add.text(140, 10, oxigenio + '%', fonte);
 
         //controlo de Temperatura
         const tempImage = this.add
             .image(200, 20, 'temperatura')
             .setScale(0.035);
-        let temperatura = 10;
-        let temperaturaText = this.add.text(220, 10, temperatura + 'ºC', fonte);
+        temperatura = 10;
+        temperaturaText = this.add.text(220, 10, temperatura + 'ºC', fonte);
 
         //controlo de dinheiro
         const moedasImage = this.add.image(290, 20, 'moedas').setScale(0.015);
-        let moedas = 20;
-        let moedasText = this.add.text(320, 10, moedas + '€', fonte);
+        moedas = 20;
+        moedasText = this.add.text(320, 10, moedas + '€', fonte);
 
         //controlo de gerações
         const geracoesImage = this.add
             .image(this.cameras.main.width - 130, 20, 'geracoes')
             .setScale(0.15);
-        let geracoes = 0;
-        let geracoesText = this.add.text(
+        geracoes = 0;
+        geracoesText = this.add.text(
             this.cameras.main.width - 100,
             10,
             geracoes + ' Gerações',
@@ -93,6 +110,13 @@ class WorldScene extends Phaser.Scene {
                 .create(treeObject.x, treeObject.y - treeObject.height, 'tree')
                 .setOrigin(0, 0);
             tree.body.setSize(tree.width, tree.height - 5).setOffset(0, 5);
+            this.physics.add.collider(
+                this.player,
+                this.treesGroup,
+                playerHit,
+                null,
+                this
+            );
         });
 
         house_hitObjects.forEach((house_hitObject) => {
@@ -106,14 +130,20 @@ class WorldScene extends Phaser.Scene {
             house_hit.body
                 .setSize(house_hit.width, house_hit.height)
                 .setOffset(0, 0);
-
             this.physics.add.collider(
                 this.player,
-                this.treesGroup,
+                this.house_hitGroup,
                 playerHit,
                 null,
                 this
             );
+        });
+
+        // Inicializar um timer
+        timer = this.time.addEvent({
+            delay: 500, // ms
+            callback: updateStatus,
+            loop: true,
         });
     }
 
@@ -142,9 +172,27 @@ class WorldScene extends Phaser.Scene {
 
             this.player.play('idle', true);
         }
+
+        vidaText.text = vida + 'HP';
+        oxigenioText.text = oxigenio + '%';
+        temperaturaText.text = temperatura + 'ºC';
+        moedasText.text = moedas + '€';
+        geracoesText.text = geracoes + ' Gerações';
+
+        updateStatusBar();
     }
 }
 
 function playerHit(player, tree) {
     console.log('bateu');
 }
+
+function updateStatus() {
+    vida--;
+    oxigenio -= 2;
+    if (vida == 0) {
+        this.game.state.restart();
+    }
+}
+
+function updateStatusBar() {}
